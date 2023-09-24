7e77d92f2d2f1fc05aebf4d6050817ad238381f0
Noam <noamru22@gmail.com>
2023-09-24 09:23:38 -0400

    new deployment

--- src/components/dashboard/TruthMeasure.tsx
+++ src/components/dashboard/TruthMeasure.tsx
@@ -13,11 +13,13 @@ export const TruthMeasure: React.FC<TruthMeasureProps> = ({
     <div className="flex flex-col gap-6 align-center items-center m-3 ">
       <div
         className={`radial-progress border-4 border-neutral-800 ${colorClass}`}
-        style={{
-          "--value": truthPercentage, // looks like an error but it's not
-          "--thickness": "1rem",
-          "--size": "10rem",
-        }}
+        style={
+          {
+            // "--value": truthPercentage, // looks like an error but it's not
+            // "--thickness": "1rem",
+            // "--size": "10rem",
+          }
+        }
       >
         {truthPercentage}%
       </div>
