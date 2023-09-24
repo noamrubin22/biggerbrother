8aab99ae5560b6e18246b7f1e0b035001eed31a8
Rasha Saadeh <>
2023-09-24 06:44:55 -0400

    add in form

--- src/components/dashboard/Form.tsx
+++ src/components/dashboard/Form.tsx
@@ -1,9 +1,86 @@
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import { createHelia } from 'helia'
+import { json } from '@helia/json'
+import lighthouse from "@lighthouse-web3/sdk";
 
 export const Form = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const router = useRouter();
+
+  // Step 1: Define state variables for form input values
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    image: null, // For file input, initialize as null
+  });
+
+
+  // Step 2: Event handlers to update the state when form fields change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFormData({
+      ...formData,
+      image: file,
+    });
+  };
+
+
+
+  const handleAddPolitician = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e)
+    const helia = await createHelia()
+    const j = json(helia)
+
+    // Here, you can access the form data from the formData state variable
+    console.log('Form Data:', formData.name);
+    // const form = e.currentTarget;
+    // console.log(form)
+    // const formData = new FormData(form);
+    // const { name, description, image } = Object.fromEntries(formData.entries());
+    // console.log("RAH", name, description, image);
+    // Convert and add personObject to IPFS
+    const politicianCID = await j.add({
+      name: formData.name,
+      description:formData.description,
+      image: formData.image
+    })
+
+    console.log("politicianCID toString()", politicianCID.toString())
+    console.log("politicianCID toJSON()", politicianCID.toJSON())
+    console.log("politicianCID link()", politicianCID.link())
+
+
+    // TODO:  call the addPolitician ABI 
+  };
+
+  const handleAddEvidence = async (e: React.FormEvent<HTMLFormElement>) => {
+    // Convert and add personObject to IPFS
+    const evidenceCID = await lighthouse.upload('/path/to/adorable/dog.jpg', 'YOUR_API_KEY');
+    console.log("evidenceCID", evidenceCID)
+    // TODO:  call the addEvidence ABI 
+  };
+
+
+  // retireves a mapping of pCID and eCID data 
+  const handleSmartContractData = async () => {
+
+    // TODO:  call thepublic mapping in the smart contract
+
+    //https://github.com/ipfs/helia#-json
+    // can loop through the mapp and uncompresse the mapping object
+    // const retrievedObject = await d.get(myImmutableAddress2)
+    // console.log(retrievedObject)
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -23,12 +100,7 @@ export const Form = () => {
   return (
     <div className="font-mono">
       <h1 className="text-3xl text-center">Add a politician</h1>
-      <form
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-        className="mt-3 "
-      >
+      <form onSubmit={handleAddPolitician} className="mt-3 ">
         <div className="w-full">
           <label className="flex flex-col mb-4">
             <p className="label-text text-xs my-2">Name</p>
@@ -37,6 +109,8 @@ export const Form = () => {
               name="name"
               placeholder="Joe Biden"
               className="input input-bordered w-full max-w-xs"
+              value={formData.name}
+              onChange={handleInputChange}
             />
           </label>
         </div>
@@ -47,6 +121,8 @@ export const Form = () => {
             name="description"
             placeholder="President"
             className="input input-bordered w-full max-w-xs"
+            value={formData.description}
+            onChange={handleInputChange}
           />
         </label>
         <div className="form-control w-full max-w-xs mt-3">
@@ -57,6 +133,7 @@ export const Form = () => {
             type="file"
             name="image"
             className="file-input file-input-bordered w-full max-w-xs"
+            onChange={handleFileChange}
           />
         </div>
         <div className="flex justify-start">
