1a2c51cc2044fee6cd5f14dd55df7bde52c20e45
Samuel Gbafa <skgbafa@gmail.com>
2023-09-24 05:31:22 -0400

    added uma support

--- hh/contracts/BB.sol
+++ hh/contracts/BB.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "https://github.com/UMAprotocol/protocol/blob/7a93650a7494eaee83756382a18ecf11314499cf/packages/core/contracts/optimistic-oracle-v3/interfaces/OptimisticOracleV3Interface.sol";
+
 contract BiggerBrother {
     // Array to hold all politician ids
     bytes32[] public politicians;
-    enum Status { PENDING, SUCCESS, FAIL }
+    enum Status {PENDING, SUCCESS, FAIL}
 
     struct Evidence {
         bytes32 ipfs_data_cid;  // array of cid of json objects with evidence data
         Status uma_status;
+        bytes32 assertionId;
     }
 
     // Define a public mapping from bytes32 to an array of bytes32
@@ -28,6 +31,24 @@ contract BiggerBrother {
     // Function to push a value to the array associated with a key
     function addNewEvidence(bytes32 pCID, bytes32 eCID) public {
         Evidence newEvidence = new Evidence(eCID, Status.PENDING);
+
+        // get assertion
+        string memory pCIDString = string(abi.encodePacked(pCID));
+        string memory eCIDString = string(abi.encodePacked(eCID));
+        string assertionString = string.concat("The following evidence is true for the politician. IPFS hashes: [Politician: ", pCIDString, "] [Evidence: ", eCIDString, "].");
+        bytes assertion = bytes(assertionString);
+
+        // assert against the Optimistic Asserter
+        newEvidence.assertionId = oov3.assertTruthWithDefaults(assertion, address(this));
+
+        // add evidence to mapping
         politicianToEvidenceMapping[pCID].push(newEvidence);
     }
+
+    function settleAndGetAssertionResult(bytes32 pCID, bytes32 eCID) public returns (bool) {
+        bytes32 assertionId = politicianToEvidenceMapping[pCID][eCID].assertionId;
+        politicianToEvidenceMapping[pCID][eCID].uma_status =  oov3.settleAndGetAssertionResult(assertionId);
+    }
+
+
 }
